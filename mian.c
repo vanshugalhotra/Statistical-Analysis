09263f15b5dcad5f9634baad721af9b491a1c247
Vanshu <vanshugalhotra3332@gmail.com>
2021-11-25 16:10:09 +0530

    added functions to find mean for both series

--- mian.c
+++ mian.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <conio.h>
 
+float ind_mean(float series[], int size) // sigma X / n
+{
+    float sum = 0, mean;
+    for (int i = 0; i < size; i++)
+    {
+        sum += series[i];
+    }
+    mean = sum / size;
+    return mean;
+}
+
+float disc_mean(float series[][2], int size) // sigma Fx/ sigma F
+{
+    float sum = 0, mean, sigmaf=0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += series[i][0] * series[i][1];
+        sigmaf+= series[i][1];
+    }
+    mean = sum / sigmaf;
+    return mean;
+}
+
 int main()
 {
     int opt, ser, elm;
+    float mean, median, mode;
     printf("------Basic Statistical Operations-----------------\n");
 
     printf("Which Type of Series?\n1.) Individual\n2.) Discrete\n");
@@ -21,7 +45,8 @@ int main()
     case 1:
         printf("Enter the Elements of the Series: (x)\n");
         for (int i = 0; i < elm; i++)
-        {
+        {   
+            printf("Enter the value of X: (%d) element\n", i+1);
             scanf("%f", &ind_ser[i]);
         }
         break;
@@ -29,9 +54,11 @@ int main()
     case 2:
         printf("First Enter The Element and Then Its Frequency:(x-f) \n");
         for (int i = 0; i < elm; i++)
-        {
+        {   
+            printf("Enter the value of X: (%d) element\n", i+1);
             scanf("%f", &disc_ser[i][0]); // for X
-            scanf("%d", &disc_ser[i][1]);  // for F
+            printf("Enter the value of F: (%d) element\n", i+1);
+            scanf("%d", &disc_ser[i][1]); // for F
         }
         break;
 
@@ -49,12 +76,38 @@ int main()
         switch (opt)
         {
         case 1:
+            if (ser == 1)
+            {
+                mean = ind_mean(ind_ser, elm);
+                printf("The mean is %f\n", mean);
+            }
+
+            else if (ser == 2)
+            {
+                mean = disc_mean(disc_ser, elm);
+                printf("The mean is %f\n", mean);
+            }
+
             break;
 
         case 2:
+            if (ser == 1)
+            {
+            }
+
+            else if (ser == 2)
+            {
+            }
             break;
 
         case 3:
+            if (ser == 1)
+            {
+            }
+
+            else if (ser == 2)
+            {
+            }
             break;
 
         default:
